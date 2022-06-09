97bf64f66e3dcf3615ebc5da92e67a2e51b9690f
mica-alex <83238954+mica-alex@users.noreply.github.com>
2022-06-09 16:19:51 -0400

    Update version

--- src/main/java/com/micatechnologies/minecraft/launcher/consts/LauncherConstants.java
+++ src/main/java/com/micatechnologies/minecraft/launcher/consts/LauncherConstants.java
@@ -43,7 +43,7 @@ public class LauncherConstants
      *
      * @since 1.0
      */
-    public final static String LAUNCHER_APPLICATION_VERSION = "2021.3.1";
+    public final static String LAUNCHER_APPLICATION_VERSION = "2021.3.2";
 
     /**
      * Launcher application name without spaces.
