994bdaf75ff9529a1a10c0b190dc085e7127fe44
mica-alex <83238954+mica-alex@users.noreply.github.com>
2024-07-24 15:35:34 -0400

    Improve fileUtils.ts

--- src/main/utils/fileUtil.ts
+++ src/main/utils/fileUtil.ts
@@ -1,87 +1,77 @@
-import fs from 'fs';
-import path from 'path';
-import { Mutex } from 'async-mutex';
+import { promises as fs } from 'fs'
+import path from 'path'
+import { Mutex } from 'async-mutex'
 
-let baseDirectory: string = process.cwd();
-const baseDirectoryMutex = new Mutex();
+let baseDirectory: string = process.cwd()
+const baseDirectoryMutex = new Mutex()
 
 export async function getBaseDirectory(): Promise<string> {
-    return baseDirectoryMutex.runExclusive(() => {
-        return baseDirectory;
-    });
+  return baseDirectoryMutex.runExclusive(() => {
+    return baseDirectory
+  })
 }
 
 export async function setBaseDirectory(directory: string): Promise<void> {
-    return baseDirectoryMutex.runExclusive(() => {
-        baseDirectory = directory;
-    });
+  return baseDirectoryMutex.runExclusive(() => {
+    baseDirectory = directory
+  })
 }
 
-export function readFile(filePath: string): Promise<string> {
-    const absolutePath = path.join(baseDirectory, filePath);
-    return new Promise((resolve, reject) => {
-        fs.readFile(absolutePath, 'utf8', (err, data) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(data);
-            }
-        });
-    });
+export async function readFileAsString(
+  filePath: string,
+  encoding: BufferEncoding = 'utf8'
+): Promise<string> {
+  const baseDirectory = await getBaseDirectory()
+  const absolutePath = path.join(baseDirectory, filePath)
+  return await fs.readFile(absolutePath, encoding)
 }
 
-export function writeFile(filePath: string, content: string): Promise<void> {
-    const absolutePath = path.join(baseDirectory, filePath);
-    return new Promise((resolve, reject) => {
-        fs.writeFile(absolutePath, content, 'utf8', (err) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve();
-            }
-        });
-    });
+export async function writeFileAsString(
+  filePath: string,
+  content: string,
+  encoding: BufferEncoding = 'utf8'
+): Promise<void> {
+  const baseDirectory = await getBaseDirectory()
+  const absolutePath = path.join(baseDirectory, filePath)
+  await fs.writeFile(absolutePath, content, encoding)
 }
 
-export function overwriteFile(filePath: string, content: string): Promise<void> {
-    // Overwrite is essentially the same as writeFile in this context
-    return writeFile(filePath, content);
+export async function overwriteFileAsString(
+  filePath: string,
+  content: string,
+  encoding: BufferEncoding = 'utf8'
+): Promise<void> {
+  // Overwrite is essentially the same as writeFile in this context
+  return writeFileAsString(filePath, content, encoding)
 }
 
-export function deleteFile(filePath: string): Promise<void> {
-    const absolutePath = path.join(baseDirectory, filePath);
-    return new Promise((resolve, reject) => {
-        fs.unlink(absolutePath, (err) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve();
-            }
-        });
-    });
+export async function deleteFile(filePath: string): Promise<void> {
+  const absolutePath = path.join(baseDirectory, filePath)
+  await fs.unlink(absolutePath)
 }
 
 // JSON-specific functions
 
-export async function readJsonFile<T>(filePath: string): Promise<T> {
-    const data = await readFile(filePath);
-    try {
-        return JSON.parse(data);
-    } catch (error: unknown) {
-        if (error instanceof Error) {
-            throw new Error(`Invalid JSON in file ${filePath}: ${error.message}`);
-        } else {
-            throw new Error(`Invalid JSON in file ${filePath}`);
-        }
-    }
+export async function readFileAsJson<T>(filePath: string): Promise<T> {
+  const data = await readFileAsString(filePath)
+  return JSON.parse(data)
 }
 
-export async function writeJsonFile<T>(filePath: string, jsonObject: T, indentSize?: number): Promise<void> {
-    const content = JSON.stringify(jsonObject, null, indentSize ?? 0); // Pretty-print JSON with specified indent size or minimized
-    await writeFile(filePath, content);
+export async function writeFileAsJson<T>(
+  filePath: string,
+  jsonObject: T,
+  indentSize: number = 0
+): Promise<void> {
+  // Pretty-print JSON with specified indent size or minimized if indent size is 0
+  const content = JSON.stringify(jsonObject, null, indentSize)
+  await writeFileAsString(filePath, content)
 }
 
-export async function overwriteJsonFile<T>(filePath: string, jsonObject: T, indentSize?: number): Promise<void> {
-    // Overwrite is essentially the same as writeJsonFile in this context
-    await writeJsonFile(filePath, jsonObject, indentSize);
+export async function overwriteJsonFile<T>(
+  filePath: string,
+  jsonObject: T,
+  indentSize: number = 0
+): Promise<void> {
+  // Overwrite is essentially the same as writeJsonFile in this context
+  await writeFileAsJson(filePath, jsonObject, indentSize)
 }
