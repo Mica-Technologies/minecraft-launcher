d768a6052f65b5e21cefb7cec3efab1bd0b2cac3
mica-alex <83238954+mica-alex@users.noreply.github.com>
2024-10-10 12:51:09 -0400

    Update/install MUI

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,15 +1,20 @@
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import Main from './pages/Main';
 import About from './pages/About';
 
 function App(): JSX.Element {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </HashRouter>
+    <React.Fragment>
+      <CssBaseline />
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </HashRouter>
+    </React.Fragment>
   );
 }
 
--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,4 +1,8 @@
 import './assets/main.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
