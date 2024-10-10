4174737a55476ba58e12694765f3b23d204afc92
mica-alex <83238954+mica-alex@users.noreply.github.com>
2024-10-10 12:19:26 -0400

    Update dev env

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,5 @@ module.exports = {
     'plugin:react/jsx-runtime',
     '@electron-toolkit/eslint-config-ts/recommended',
     '@electron-toolkit/eslint-config-prettier'
-  ],
-  'prettier/prettier': ['error', { endOfLine: 'auto' }]
+  ]
 }
